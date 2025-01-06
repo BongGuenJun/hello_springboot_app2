fbcbb5702d41fbb53448ada9ca9d715bcb1748e3
penguindisc <n84066@gmail.com>
2025-01-06 12:12:24 +0900

    Initial commit

---
+++ src/main/java/penguindisco/hello_spring_app2/HelloSpringApp2Application.java
@@ -0,0 +1,13 @@
+package penguindisco.hello_spring_app2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApp2Application.class, args);
+    }
+
+}
